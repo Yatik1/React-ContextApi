e577edd2272f8b7ac1395e54926e300f001f501e
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-12-21 23:51:39 +0530

    start

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./App.css"
+function App() {
+  return (
+    <div>
+       <h1>React Context Api For State Management 🚉</h1>
+    </div>
+  )
+}
+
+export default App
