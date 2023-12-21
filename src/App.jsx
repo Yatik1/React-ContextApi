9e769ca0a29a950c5eb90e672c3e9d24080f2fbe
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-12-22 00:02:15 +0530

    ::Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./App.css"
 import Login from './components/Login'
+import Profile from './components/Profile'
 function App() {
   return (
     <div>
        <h1>React Context Api Basics For State Management 🚉</h1>
        <Login />
+       <Profile />
     </div>
   )
 }
