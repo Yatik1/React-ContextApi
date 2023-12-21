8d0a238eceda786c52c522763a5046ae95cbd569
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-12-21 23:56:53 +0530

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./App.css"
+import Login from './components/Login'
 function App() {
   return (
     <div>
        <h1>React Context Api For State Management 🚉</h1>
+       <Login />
     </div>
   )
 }
